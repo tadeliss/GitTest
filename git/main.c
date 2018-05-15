e44d29afc3fbf62f7b95d7a212bb930258a9cb36
tadeliss <tads.ra@gmail.com>
2018-05-15 17:00:24 +0300

    sudas

--- git/main.c
+++ git/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int i,a[10], b=1,s,c[5];
+float av;
+
+
 int main()
 {
-    printf("Hello world!\n");
+for (i=0,b; i<5;i++,b++)
+{
+
+    printf("number %d: ", b );
+    scanf("%d", &a[i]);
+}
+    for (i=0;i<5;i++)
+    {
+        c[i]=a[i]*a[i]*a[i];
+printf("\n%d", c[i]);
+
+    }
+
     return 0;
 }
